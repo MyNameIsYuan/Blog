80f555ec8d9dc30a072f077e34a1437cc0a2f186
Zero <a958787c@qq.com>
2019-02-14 17:49:16 +0800

    初始化

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ const resolve = dir => {
 
 module.exports = {
   outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production'? '/production-sub-path/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'? '/Blog/' : '/',
   // 打包时不生成.map文件
   productionSourceMap: false,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
